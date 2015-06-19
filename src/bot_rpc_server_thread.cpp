54cc328241eae226c7e166e39783633b701399b5
Bertrand HIGY <bertrand.higy@gmail.com>
2015-06-19 16:36:25 +0200

    First commit

---
+++ src/bot_rpc_server_thread.cpp
@@ -0,0 +1,51 @@
+/* 
+ * Copyright (C) 2015 iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Bertand HIGY
+ * email:  bertrand.higy@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <yarp/os/Bottle.h>
+#include <yarp/os/Time.h>
+
+#include "bot_rpc_server_thread.h"
+
+using namespace std;
+using namespace yarp::os;
+
+BotRpcServerThread::BotRpcServerThread(RpcServer *rpc_port, string *default_answer,  Property *dictionary, double delay)
+	: rpc_port_(rpc_port), default_answer_(default_answer), dictionary_(dictionary), delay_(delay)
+{
+}
+
+void BotRpcServerThread::run()
+{
+	while (!isStopping())
+	{
+		cout << "Waiting for a message..." << endl;
+		Bottle request, reply;
+		rpc_port_->read(request, true);
+		cout << "Message: " << request.toString() << endl;
+		Time::delay(delay_);
+		if (dictionary_ != NULL)
+		{
+			Value answer = dictionary_->find(request.toString());
+			if (answer.isNull())
+				reply.addString(default_answer_->c_str());
+			else
+				reply.addString(answer.asString());
+		}
+		cout << "Reply: >>" << reply.toString() << endl;
+		rpc_port_->reply(reply);
+	}
+}
---
+++ src/bot_rpc_server_thread.h
@@ -0,0 +1,40 @@
+#ifndef AUTOMATIC_RPC_SERVER_THREAD_H
+#define AUTOMATIC_RPC_SERVER_THREAD_H
+
+/* 
+ * Copyright (C) 2015 iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Bertand HIGY
+ * email:  bertrand.higy@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <string>
+#include <yarp/os/Property.h>
+#include <yarp/os/RpcServer.h>
+#include <yarp/os/Thread.h>
+  
+class BotRpcServerThread : public yarp::os::Thread
+{
+	public:
+		BotRpcServerThread(yarp::os::RpcServer *rpc_port, std::string *default_answer,  yarp::os::Property *dictionary, double delay);   
+		void run(); 
+	   
+	private:
+		/* thread parameters */
+		std::string 		*default_answer_;
+		yarp::os::Property 	*dictionary_;
+		yarp::os::RpcServer *rpc_port_;
+		double 				delay_;
+};
+
+#endif
